dd3a0c7d36d0b841ad9305314c25bfa79f8c3b6e
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-06 21:26:50 -0300

    Segundo arquivo JS

---
+++ arquivo2.js
@@ -0,0 +1,22 @@
+const numero1 = 10    
+const numero2 = 30
+const numero3 = 2
+const numero4 = 3
+
+
+//coerção/conversão implícita
+const soma = numero1 + numero2
+const subtracao = numero1 - numero2
+const divisao = numero1 / numero2
+const multiplicacao = numero1 * numero2
+const elevado = numero3 ** numero4
+const elevado2 = Math.pow(2 ,3)
+const restoDaDivisao = 3 % 2
+
+console.log({soma})
+console.log({subtracao})
+console.log({divisao})
+console.log({multiplicacao})
+console.log({elevado})
+console.log({elevado2})
+console.log({restoDaDivisao})
