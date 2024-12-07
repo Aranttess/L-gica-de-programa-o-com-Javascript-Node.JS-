4d86c86b3e5be245b0c4346b6f570813024caa61
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-07 20:45:41 -0300

    operadores booleanos

---
+++ operadores-booleanos.js
@@ -0,0 +1,8 @@
+// or/ou : ||
+// and/e : &&
+
+// expressões com OU sempre retorna verdadeiro, a menos que TODAS as expressões sejam falsas
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
