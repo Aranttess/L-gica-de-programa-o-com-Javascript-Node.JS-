8a45160d8c23f5db7b30e2aac692f3dccb4fae13
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-06 21:46:15 -0300

    adicionando estudo sobre vetores JS

---
+++ vetores.js
@@ -0,0 +1,15 @@
+//VETORES / LISTAS
+
+const idades = [30, 24, 28]
+
+//abertura de um escopo
+{
+    const nomeDeAlunos = ['Rodrigo', 'Raquel', 'Matheus']
+    console.log(nomeDeAlunos)
+    {
+        const sexo = ['Masculino', 'Feminino', 'Masculino']
+        console.log(sexo)
+    }
+}
+
+console.log(idades)
