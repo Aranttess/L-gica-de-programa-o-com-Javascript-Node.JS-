0ab7327250e8c07d24f26990d7ec511bb36fcb80
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-06 21:25:14 -0300

    primeiro arquivo js

---
+++ arquivo.js
@@ -0,0 +1,20 @@
+// Aula introdutória
+
+let  numero = 20 // inteiro
+const numero2 = 300 // inteiro 
+let idade = 30 //inteiro
+const numeroFavorito = 250000001 // real
+const estaChovendo = false
+const qualquerCoisa = null
+
+const nome = 'Rodrigo'
+console.log(qualquerCoisa)
+console.log(numero)
+console.log(idade)
+
+
+
+console.log('Oi, ' + nome + '!')
+console.log(`Oi, meu nome é ${nome} e tenho ${idade} anos!`)
+console.log(`Acondição do clima é de sol: ${estaChovendo}`)
+console.log(`Seu número favorito é: ${numeroFavorito}`)
