89873a48f4ada4649b2fcdcd7c9321f7606ebfca
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-08 20:10:24 -0300

    arquivo operadores booleanos

--- operadores-booleanos.js
+++ operadores-booleanos.js
@@ -6,3 +6,11 @@ console.log(true || true);
 console.log(true || false);
 console.log(false || true);
 console.log(false || false);
+console.log()
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+
