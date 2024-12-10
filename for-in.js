39fbea918977feb987042d43f97a4d673a11daae
Rodrigo <rodrigoslarantes@gmail.com>
2024-12-10 10:50:42 -0300

    arquivos de estudo for

--- for-in.js
+++ for-in.js
@@ -1,13 +1,34 @@
+/*O laço for...in interage sobre propriedades enumeradas de um objeto, na ordem original de inserção.
+ O laço pode ser executado para cada propriedade distinta do objeto*/
+ //Quando devo usar for...in? Quando for manipular Objetos
+   //Quando devo usar for...of? Quando for manipular Arrays
+
+
 const users = {
     //'nome' é uma KEY(chave) e 'joSé mAria' é value(valor)
-    name: 'joSé mAria',
+    name: 'joSé mAria SOUZA LEao',
     email: 'JOSE.M1@gmail.com',
     age: 23,
     address: 'Rua jose Armando machado',
 }
 
 for (const key in users) {
-    if (key === 'name' || key === 'email') {
+    if (key === 'name') {
+        //O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array.
+        const names = users[key].split(' ')
+        users[key]= ''
+        for (const name of names) {
+            const normalizedName = name.toLocaleLowerCase();
+            const [primeiraLetra,...restoDoNome] = normalizedName
+
+            //O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.
+            users[key] = users[key] + ' ' + primeiraLetra.toUpperCase() + restoDoNome.join('')
+            users[key] = users[key].trim()
+
+        }
+        
+    }
+    if (key === 'email') {
         users[key] = users[key].toLocaleLowerCase()
     }
 }
--- for-of.js
+++ for-of.js
@@ -1,5 +1,7 @@
 /* • O loop for-of percorre objetos iterativos (incluindo Array, Map, Set, o objeto arguments e assim por diante),
  chamando uma função personalizada com instruções a serem executadas para o valor de cada objeto distinto.*/
+  //Quando devo usar for...of? Quando for manipular Arrays
+
 
  const listaDeNumeros = [33, 131, 155, 555, 222, -21, -56]
  const listaDeUsuarios = ['Rodrigo', 'Raquel']
